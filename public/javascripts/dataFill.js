c275ee0416ec1c2dd6356be6d33835ee37c12bfd
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-04-27 21:36:28 +0200

    Added styling and display improvements

--- public/javascripts/dataFill.js
+++ public/javascripts/dataFill.js
@@ -8,7 +8,18 @@ const windSpeedElement = document.querySelector("[data-speed]");
 const windDirectionElement = document.querySelector("[data-direction]");
 const sunriseElement = document.querySelector("[data-sunrise]");
 const sunsetElement = document.querySelector("[data-sunset]");
-const forecastElements = document.querySelector(".forecasts")
+const forecastElements = document.querySelector(".forecasts");
+const mainIconElement = document.querySelector(".main-icon");
+
+// // Units
+
+function displayTemperature(temp) {
+    return `${Math.round(temp)} ºC`
+}
+
+// function displaySpeed() {
+
+// }
 
 function convertTime(unixTime) {
     // Converts unix time to local time format.
@@ -20,35 +31,33 @@ function convertTime(unixTime) {
     return convertedTime;
 }
 
-// function weekDay(unixTime) {
-//     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//     const dayNum = new Date(unixTime * 1000).getDay();
-//     return days[dayNum];
-// }
-
 function setCurrent(data) {
-    cloudElement.textContent = data.clouds;
-    temperatureElement.textContent = data.temp;
-    feelsLikeElement.textContent = data.feels_like;
-    humidityElement.textContent = data.humidity;
+    cloudElement.textContent = `${data.clouds}%`;
+    temperatureElement.firstChild.nodeValue = displayTemperature(data.temp);
+    feelsLikeElement.textContent = displayTemperature(data.feels_like);
+    humidityElement.textContent = `${data.humidity}%`;
     descriptionElement.textContent = data.weather[0].description;
     windSpeedElement.textContent = data.wind_speed;
     windDirectionElement.textContent = data.wind_deg;
     sunriseElement.textContent = convertTime(data.sunrise);
     sunsetElement.textContent = convertTime(data.sunset);
+    mainIconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
 }
 
 function setForecast(data, index) {
     const [day, weekDay] = new Date(data.sunrise * 1000).toLocaleDateString('en-US', { weekday: 'long', day: "numeric" }).split(" ")
     const dailyForecast = document.createElement("div");
-    dailyForecast.setAttribute(`daily-forecast-${index}`, "")
+    // dailyForecast.setAttribute(`daily-forecast-${index}`, "")
+    dailyForecast.classList.add("daily-forecast");
     dailyForecast.innerHTML = `${weekDay} ${day}
-    <span class="forecast-max">${data.temp.max}</span>
-    <span class="forecast-min">${data.temp.min}</span>`;
+    <img class="forecast-img" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+    <span class="forecast-max">${displayTemperature(data.temp.max)}</span>
+    <span class="forecast-min">${displayTemperature(data.temp.min)}</span >`;
     forecastElements.appendChild(dailyForecast);
 }
 
 function setValues(data, location) {
+
     locationElement.textContent = location;
 
     // Current conditions
@@ -57,5 +66,6 @@ function setValues(data, location) {
     // Forecast
     forecastElements.innerHTML = ""; // reset forecast div
     data.daily.map((data, index) => setForecast(data, index));
-}
 
+    setUnits();
+}
