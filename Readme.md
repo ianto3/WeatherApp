0c73cde91c3a69068768fecdae296fe13a63e217
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-04-28 00:45:08 +0200

    Added unit conversion and more styling

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Weather App
+
+Weather App based on openweather's API.
+
+Javascript, HTML and CSS has been used alongside with Node and Express to host openweather requests.
+
+Mapbox geocoder has been implemented for location queries.
--- public/javascripts/dataFill.js
+++ public/javascripts/dataFill.js
@@ -1,25 +1,58 @@
 const locationElement = document.querySelector("[data-location]");
 const temperatureElement = document.querySelector("[data-temperature]");
 const descriptionElement = document.querySelector("[data-description]");
-const feelsLikeElement = document.querySelector("[data-feels]");
+const feelsLikeElement = document.querySelector(".feels-like");
 const cloudElement = document.querySelector("[data-clouds]");
 const humidityElement = document.querySelector("[data-humidity]");
 const windSpeedElement = document.querySelector("[data-speed]");
 const windDirectionElement = document.querySelector("[data-direction]");
+const windCompass = document.querySelector(".wind-direction")
 const sunriseElement = document.querySelector("[data-sunrise]");
 const sunsetElement = document.querySelector("[data-sunset]");
 const forecastElements = document.querySelector(".forecasts");
 const mainIconElement = document.querySelector(".main-icon");
 
-// // Units
+// Unit handling
 
-function displayTemperature(temp) {
-    return `${Math.round(temp)} ºC`
+const celsiusInput = document.querySelector("#celsius");
+const fahrenheitInput = document.querySelector("#fahrenheit")
+
+function isMetric() {
+    return celsiusInput.checked
 }
 
-// function displaySpeed() {
+function updateUnits() {
+    // Temperatures
+    const allTemperatureValues = document.querySelectorAll("[data-temperature]");
+    allTemperatureValues.forEach(el => el.textContent = displayTemperature(el.textContent));
 
-// }
+    const allTempUnits = document.querySelectorAll("[data-temp-unit]");
+    allTempUnits.forEach(el => el.textContent = isMetric() ? " ºC" : " ºF");
+
+    // Speed
+    const allSpeedValues = document.querySelectorAll("[data-speed]");
+    console.dir(allSpeedValues)
+    allSpeedValues.forEach(el => el.textContent = displaySpeed(el.textContent));
+
+    const allSpeedUnits = document.querySelectorAll("[data-speed-unit]");
+    allSpeedUnits.forEach(el => el.textContent = isMetric() ? " km/h" : " mph");
+}
+
+function displayTemperature(temp) {
+    if (isMetric()) {
+        return Math.round(temp);
+    }
+    return Math.round(temp * (9 / 5) + 32);
+}
+
+function displaySpeed(speed) {
+    if (isMetric()) {
+        return speed;
+    }
+    return (speed / 1.609).toFixed(2);
+}
+
+// Data display
 
 function convertTime(unixTime) {
     // Converts unix time to local time format.
@@ -37,8 +70,9 @@ function setCurrent(data) {
     feelsLikeElement.textContent = displayTemperature(data.feels_like);
     humidityElement.textContent = `${data.humidity}%`;
     descriptionElement.textContent = data.weather[0].description;
-    windSpeedElement.textContent = data.wind_speed;
+    windSpeedElement.textContent = displaySpeed(data.wind_speed);
     windDirectionElement.textContent = data.wind_deg;
+    windCompass.style.transform = `rotate(${data.wind_deg + 90}deg)`
     sunriseElement.textContent = convertTime(data.sunrise);
     sunsetElement.textContent = convertTime(data.sunset);
     mainIconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
@@ -47,25 +81,39 @@ function setCurrent(data) {
 function setForecast(data, index) {
     const [day, weekDay] = new Date(data.sunrise * 1000).toLocaleDateString('en-US', { weekday: 'long', day: "numeric" }).split(" ")
     const dailyForecast = document.createElement("div");
-    // dailyForecast.setAttribute(`daily-forecast-${index}`, "")
     dailyForecast.classList.add("daily-forecast");
+
     dailyForecast.innerHTML = `${weekDay} ${day}
     <img class="forecast-img" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
-    <span class="forecast-max">${displayTemperature(data.temp.max)}</span>
-    <span class="forecast-min">${displayTemperature(data.temp.min)}</span >`;
+    <div class="forecast-max">
+    <span data-temperature>${displayTemperature(data.temp.max)}</span>
+    <span data-temp-unit> ºC</span>
+    </div>
+    <div class="forecast-min">
+    <span data-temperature>${displayTemperature(data.temp.min)}</span>
+    <span data-temp-unit> ºC</span>
+    </div>`;
+
     forecastElements.appendChild(dailyForecast);
 }
 
+// Main function
+
 function setValues(data, location) {
-
     locationElement.textContent = location;
-
     // Current conditions
     setCurrent(data.current);
-
     // Forecast
     forecastElements.innerHTML = ""; // reset forecast div
     data.daily.map((data, index) => setForecast(data, index));
 
-    setUnits();
+    celsiusInput.addEventListener("change", () => {
+        setCurrent(data.current);
+        forecastElements.innerHTML = ""; // reset forecast div
+        data.daily.map((data, index) => setForecast(data, index));
+        updateUnits()
+    });
+
+    fahrenheitInput.addEventListener("change", updateUnits);
 }
+
